7cc05e4828517ddaee6630af7cb3029f7c254458
UycGitHub <umut.yusuf.cinar.mail@gmail.com>
2024-09-18 13:14:18 +0300

    Added: developYoursAppsLayout/aboutLayouts LoadTheXmlResource.kt

---
+++ app/src/main/java/com/umutyusufcinar/xmlviewstraining/LoadTheXmlResource.kt
@@ -0,0 +1,17 @@
+package com.umutyusufcinar.xmlviewstraining
+/*
+    Just like the MainActivity.kt file, we need to create a new Kotlin file for the LoadTheXmlResource.kt file.
+    This file created for code practice purposes. If you want to see the reel code, please check the MainActivity.kt file.
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+fun onCreate(savedInstance : Bundle){
+    super.onCreate(savedInstance)
+    setContentView(R.layout.write_the_xml)
+}
+
+ */
